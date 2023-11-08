f61d463672aba9d1e4d94029981f37e6e9fe3a85
nicolvisser <nicolvisser@yahoo.com>
2023-11-08 16:41:42 +0000

    fix requirements and torch hub

--- hubconf.py
+++ hubconf.py
@@ -2,5 +2,5 @@ dependencies = ["torch", "torchaudio", "sklearn", "matplotlib"]
 from gslm import GSLM  # noqa: E402
 
 
-def gslm(n_units: int = 500, dp_lmbda: int = 0) -> GSLM:
-    return GSLM(n_units=n_units, dp_lambda=dp_lmbda)
+def gslm(n_units: int = 500, dp_lambda: int = 0) -> GSLM:
+    return GSLM(n_units=n_units, dp_lambda=dp_lambda)
